154a99ba49e0be95a3f867e4389f8a7b299d6cee
dangbnhorus <dangbn@horusent.com>
2022-05-09 11:55:58 +0700

    9522

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -79,6 +79,8 @@
                     </ul>
                 </div>
             </nav>
+            <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
+            </form>
             <!-- Navbar-->
             <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
                 <li class="nav-item dropdown">
