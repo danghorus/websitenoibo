30dda5963308cf98cb2ac9e8f71c8f686dee5eb7
dangbnhorus <dangbn@horusent.com>
2022-05-17 15:52:41 +0700

    wage-bonus

--- app/Http/Controllers/Backend/TimeKeepingController.php
+++ app/Http/Controllers/Backend/TimeKeepingController.php
@@ -35,6 +35,16 @@ class TimeKeepingController extends Controller
         $users = User::all();
         return view('timekeeping.report', compact('users'));
     }
+    public function wage(Request $request)
+    {
+        $users = User::all();
+        return view('timekeeping.wage', compact('users'));
+    }
+    public function bonus(Request $request)
+    {
+        $users = User::all();
+        return view('timekeeping.bonus', compact('users'));
+    }
 
     public function get(Request $request) {
         $filters = $request->all();
