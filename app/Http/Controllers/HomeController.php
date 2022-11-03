d6877fb101412207ba2fbeba403a2a4464e63447
dangbnhorus <dangbn@horusent.com>
2022-11-03 14:36:18 +0700

    03112022

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -36,16 +36,17 @@ class HomeController extends Controller
 		$userId = Auth::user()->id;
 		$petitions01 = Petition::where('petition_status', 1)->where('user_id', '=', $userId)->get();
         
+        $birthday_month = 0;
+
                 foreach ($users as $user) {
 
-                    $user->birthday_month = 0;
-                    $this_month = date('m', strtotime(time()));
+                    $this_month = date('m', time());
                     $birthday = date('m', strtotime($user->birthday));
-                    if( $birthday == $this_month){
-                            $user->birthday_month += 1;
+                    if( $birthday ==  $this_month ){
+                            $birthday_month ++;
+                            $user->birthday_month = $birthday_month;
                     }
                 }
-
 
         return view('home', compact('users', 'projects', 'petitions', 'petitions1', 'petitions01'));
     }
